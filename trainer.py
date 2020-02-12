38e7b47b1422b0b33ebb618751a2ad96bad22537
Masuda <masuda@Masudas-MacBook-Pro.local>
2020-02-12 13:50:50 +0900

    add libs

--- trainer.py
+++ trainer.py
@@ -8,4 +8,20 @@ def train(G, D, dataloader, num_epochs):
     g_lr, d_lr = 0.0001, 0.0004
     beta1, beta2 = 0.0, 0.9
 
-    g_optimizer = 
+    g_optimizer = torch.optim.Adam(G.parameters(), g_lr, [beta1, beta2])
+    d_optimizer = torch.optim.Adam(D.parameters(), d_le, [beta1, beta2])
+
+    criterion = nn.BCEWithLogitsLoss(reduction='mean')
+
+    z_dim = 20
+    mini_batch_size = 64
+
+    G.to(device)
+    D.to(device)
+
+    G.train()
+    D.train()
+
+    torch.backends.cudnn.benchmark = True
+
+    num_train_imgs = 
